44d16c879017be7134c9afdb350c3b3e38ef3627
Gabriel Schorsch Amadeu <gabrielschorsch@outlook.com>
2019-11-01 14:30:50 -0300

    .

---
+++ Mobile/cleveland/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import { View, FlatList } from 'react-native';
+
+// import { Container } from './styles';
+
+export default class cleveland extends Component {
+  render() {
+    return (
+      <View>
+        <FlatList renderItem={({item}) => {
+          
+        }}/>
+         
+      </View>
+    );
+  }
+}
